c8ea9b4a4dfaac84d6dc45efc5c7dd4379691886
Apoxys <apoxys@hotmail.fr>
2025-01-31 11:13:12 +0100

    corrected test logic

--- tests/model_test.py
+++ tests/model_test.py
@@ -1,24 +1,25 @@
 import unittest
 import pickle
+import joblib
 import os
 import numpy as np
 
 class TestRegressionModel(unittest.TestCase):
-    MODEL_PATH = "model_to_keep/tf_idf_LR/tf_idf_LR_model.pkl"
-    VECTORIZER_PATH = "model_to_keep/tf_idf_LR/tfidf_vectorizer.pkl"
-    VOCAB = "model_to_keep/tf_idf_LR/tfidf_vocab"
+    MODEL_PATH = "./model_to_keep/tf_idf_LR_model.pkl"
+    VECTORIZER_PATH = "./model_to_keep/tfidf_vectorizer.pkl"
+    VOCAB = "./model_to_keep/tf_idf_LR/tfidf_vocab"
     TEST_SENTENCE = "This was amazing."
 
     def setUp(self):
         #Loading model
         if not os.path.exists(self.MODEL_PATH) or not os.path.exists(self.VECTORIZER_PATH):
-            self.fail("Model, vectorizer or vocabulary is missing from path. Check path again")
+            self.fail("Model or vectorizer is missing from path. Check path again")
 
         with open(self.MODEL_PATH, 'rb') as model_file:
-            self.model = pickle.load(model_file)
+            self.model = joblib.load(model_file)
 
         with open(self.VECTORIZER_PATH, 'rb') as vectorizer_file:
-            self.vectorizer = pickle.load(vectorizer_file)
+            self.vectorizer = joblib.load(vectorizer_file)
 
     def test_prediction_consistency(self):
         #checking predictions stability
